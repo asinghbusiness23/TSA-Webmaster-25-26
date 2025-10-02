04375f548afce9d7ab5f85c4c6aa0e2d46897645
asingh099 <asingh01@student.dasd.org>
2025-10-02 14:14:11 -0400

    Improved navbar and hero section

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,17 +4,25 @@ import { Link } from "next/link";
 const Navbar = () => (
   <nav className="w-full flex items-center justify-between border-b-2 border-gray-200 h-18 text-gray-700 bg-white">
     <div className="flex items-center">
+      <div className="flex items-center">
         <div className="bg-blue-50 w-10 h-10 ml-10 flex items-center justify-center mr-2 rounded-xl">
-            <p className="text-xl text-blue-400">CC</p>
+          <p className="text-xl text-blue-400">CC</p>
         </div>
-      <h1 className="text-xl ml-0.5 font-bold">Chester County Hub</h1>
+        <h1 className="text-xl ml-0.5 font-bold">Chester County Hub</h1>
+      </div>
+      <div className="flex text-lg gap-6 mr-5 ml-4">
+        <p className="hover:text-black hover:-translate-y-0.5 duration-200 cursor-pointer bg-yellow-300 px-2 py-2 rounded-xl">
+          Home
+        </p>
+        <p className="hover:text-black hover:-translate-y-0.5 duration-200 cursor-pointer py-2 rounded-xl">
+          Resources
+        </p>
+        <p className="hover:text-black hover:-translate-y-0.5 duration-200 py-2 cursor-pointerrounded-xl">
+          Calendar
+        </p>
+      </div>
     </div>
-    <div className="flex text-lg gap-6 mr-5">
-      <p className="hover:text-black hover:-translate-y-0.5 duration-200 cursor-pointer">Home</p>
-      <p>Resources</p>
-      <p>Add</p>
-      <p>Contact</p>
-    </div>
+    <button className="btn">Submit A Resource</button>
   </nav>
 );
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -23,8 +23,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased bg-gray-600`}>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} antialiased bg-gray-100/100 h-full`}>
         {children}
       </body>
     </html>
